cee50a64ff40021323d29080c6d5ba4a5901948f
hkesari <76158363+kesarihardik@users.noreply.github.com>
2024-01-16 00:12:48 +0530

    revised

---
+++ src/Practice.java
@@ -0,0 +1,19 @@
+
+interface Car{
+    void run();
+}
+
+
+public class Practice {
+
+    public static void main(String[] args){
+        boolean x =  true;
+        Integer y = 4;
+
+        Car c = ()->System.out.println("Runs");
+        c.run();;
+
+
+
+    }
+}
